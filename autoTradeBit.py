49241bc67701a1f2b001f1e8845cbfd0bf61daa2
Kimmin-woo <ggonhu@naver.com>
2021-04-26 22:55:14 +0900

    [1111]

--- autoTradeBit.py
+++ autoTradeBit.py
@@ -6,7 +6,7 @@ import requests
 # K뱅크 값
 access = "cHJjMwVsbxZjr98OVPA2smVsvAGjg7wpP5BIeQuC"
 secret = "AXh3HuuyfYsOZipUOjkZ0daZvnD0lZVSrX1cR7Sp"
-myToken = "xoxb-1730814337234-1985015754823-p9dzNjy4kqIVmZnJvgKItsEo"
+myToken = "xoxb-1730814337234-1985015754823-H9yrKQBsUuGFynXeK16A2NSF"
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
