c96f735212786ec9c0780bcbcb40d1ae45726ac4
Kimmin-woo <ggonhu@naver.com>
2021-05-18 23:45:28 +0900

    [11]

--- autoTradeBit.py
+++ autoTradeBit.py
@@ -6,7 +6,7 @@ import requests
 # K뱅크 값
 access = "cHJjMwVsbxZjr98OVPA2smVsvAGjg7wpP5BIeQuC"
 secret = "AXh3HuuyfYsOZipUOjkZ0daZvnD0lZVSrX1cR7Sp"
-myToken = "xoxb-1730814337234-1985015754823-qCldATPEfjonoPmgEpGxVWoE"
+myToken = "xoxb-1730814337234-1985015754823-KtITeHhx1pOVpZZfNyLy1zZw"
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
@@ -20,10 +20,10 @@ def get_target_price(ticker, k):
 
     # 전날데이터를 가져옵니다.
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    # 매수목표가 = 시작가 * 1.049
-    before_target_price = df.iloc[0]['close'] * 1.049
-    # 매수목표가 = 시작가 * 1.052
-    after_target_price = df.iloc[0]['close'] * 1.052 
+    # 매수목표가 = 시작가 * 1.009
+    before_target_price = df.iloc[0]['close'] * 1.009
+    # 매수목표가 = 시작가 * 1.012
+    after_target_price = df.iloc[0]['close'] * 1.012 
     # 종가(시작가)
     start_price = df.iloc[0]['close']
 
@@ -31,12 +31,12 @@ def get_target_price(ticker, k):
 
 def get_sell_price(ticker, spay):
     """매도 목표가 조회"""
+    # 매도목표가 = 시작가 * 0.99
+    sell_price2 = spay * 0.99
     # 매도목표가 = 시작가 * 1.03
-    sell_price2 = spay * 1.03
-    # 매도목표가 = 시작가 * 1.09
-    sell_price9 = spay * 1.09
+    sell_price8 = spay * 1.03
 
-    return sell_price2, sell_price9
+    return sell_price2, sell_price8
 
 def get_start_time(ticker):
     """시작 시간 조회"""
@@ -71,6 +71,10 @@ tickers = pyupbit.get_tickers()
 symbol_list = []
 bought_list = []
 buy_list = []
+sell_krw = 0
+buy_krw = 0
+total_krw = 0
+
 for ticker in tickers:
     if 'KRW-' in ticker:
         try:
@@ -80,7 +84,7 @@ for ticker in tickers:
             df = None
 
         if df is not None:
-            if 100 < df.iloc[0]['close'] < 1000:
+            if 10 < df.iloc[0]['close'] < 10000:
                 symbol_list.append(ticker)
 
 ###################################
@@ -107,7 +111,7 @@ while True:
             #print("end_time : ", end_time - datetime.timedelta(seconds=10))
 
             # 오늘 9시 < 현재 < 내일 8시59분
-            if start_time < datetime.datetime.now() < end_time - datetime.timedelta(seconds=10):
+            if start_time < datetime.datetime.now() < end_time - datetime.timedelta(seconds=60):
 
                 before_target_price, after_target_price, start_price = get_target_price(code, 0.5)
                 current_price = get_current_price(code)
@@ -118,48 +122,49 @@ while True:
                 # 매도로직
                 if any(code in volvo for volvo in buy_list):
 
-                    sell_price2, sell_price9 = get_sell_price(code, start_price)
-                    # 시작가 <= 현재가 * 1.03 or 시작가 * 1.09 >= 현재가
-                    if current_price <= sell_price2 or current_price >= sell_price9:
+                    sell_price2, sell_price8 = get_sell_price(code, start_price)
 
-                        if current_price <= sell_price2:
-                            print("3% 매도시작")
-                            post_message(myToken,"#volvobit", "`3% 매도`")
-
-                        if current_price >= sell_price9:
-                            print("9% 매도시작")
-                            post_message(myToken,"#volvobit", "`9% 매도`")
+                    # 시작가 <= 현재가 * 0.09 or 시작가 * 1.03 >= 현재가
+                    if current_price <= sell_price2 or current_price >= sell_price8:
 
                         sell_result = upbit.sell_market_order(code, upbit.get_balance(code))
-                        post_message(myToken,"#volvobit", "매도완료, 종목 : " + code + ", 가격 : " + str(current_price))
-                        upbitYn = 'N'
+                        sell_krw = upbit.get_balance("KRW")
+                        post_message(myToken,"#volvobit", "매도완료, 종목 : " + code + ", 잔고 : " + str(sell_krw))
+                        total_krw = buy_krw-sell_krw
 
-                        index = 0
-                        for minwoo in buy_list:    
-                            if minwoo == code:
-                                del buy_list[index]
-                            index = index + 1      
+                        if current_price <= sell_price2:
+                            print("-1% 매도시작")
+                            post_message(myToken,"#volvobit", "`-1% 매도, 손해 : " + str(total_krw) + "`")
+
+                        if current_price >= sell_price8:
+                            print("3% 매도시작")
+                            post_message(myToken,"#volvobit", "`3% 매도, 이익 : " + str(total_krw) + "`")
+
+                        upbitYn = 'N'
+                        buy_list = []
+                        buy_krw = 0
+                        sell_krw = 0
+                        total_krw = 0
 
                 # 금일 매수한 종목은 매수하지 않습니다.
-                if code in bought_list: 
+                if code in bought_list:
                     continue
 
                 # 매수로직
                 if upbitYn == 'N':
                     if before_target_price < current_price < after_target_price:
                         print("매수시작 : ", code)
-                        krw = upbit.get_balance("KRW")
-                        print("krw : ", krw)
-                        buy_result = upbit.buy_market_order(code, krw-3000)
-                        post_message(myToken,"#volvobit", "매수완료, 종목 : " + code + ", 가격 : " + str(current_price))
+                        buy_krw = upbit.get_balance("KRW")
+                        post_message(myToken,"#volvobit", "매수완료, 종목 : " + code + ", 잔고 : " + str(buy_krw))
+                        buy_result = upbit.buy_market_order(code, buy_krw-2500)
                         buy_list.append(code)
                         bought_list.append(code)
-                        upbitYn = 'Y'                
+                        upbitYn = 'Y'
 
-            else:
-                bought_list = []
+                else:
+                    bought_list = []
 
-        time.sleep(1)
+            time.sleep(1)
 
     except Exception as e:
         #print(e)
