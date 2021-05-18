29042ce804357247087da9baefc1c87cc219bc0d
Kimmin-woo <83177399+Kimmin-woo@users.noreply.github.com>
2021-05-19 00:22:35 +0900

    Update autoTradeBit.py

--- autoTradeBit.py
+++ autoTradeBit.py
@@ -6,7 +6,7 @@ import requests
 # K뱅크 값
 access = "cHJjMwVsbxZjr98OVPA2smVsvAGjg7wpP5BIeQuC"
 secret = "AXh3HuuyfYsOZipUOjkZ0daZvnD0lZVSrX1cR7Sp"
-myToken = "xoxb-1730814337234-1985015754823-KtITeHhx1pOVpZZfNyLy1zZw"
+myToken = "xoxb-1730814337234-1985015754823-XoNVFSxBJebAqJ4T1R7ogFbN"
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
