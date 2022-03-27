2b72aa7ae4bd0501f416b24cbe27ef6e33192707
Kimmin-woo <83177399+Kimmin-woo@users.noreply.github.com>
2022-03-27 14:47:54 +0900

    Update autoTradeBit7.py

--- autoTradeBit7.py
+++ autoTradeBit7.py
@@ -213,7 +213,7 @@ while True:
                     if before_target_price < current_price < after_target_price:
                         predicted_close_price = predict_price(code, "2")
                         code_close_price = ((predicted_close_price/current_price)-1)*100
-                        post_message(myToken,"#volvobit", "`대상종목 : "+ code + ", 예상수익율 : " + str(round(code_close_price,1)) + "%`")
+                        #post_message(myToken,"#volvobit", "`대상종목 : "+ code + ", 예상수익율 : " + str(round(code_close_price,1)) + "%`")
                         if code_close_price > 2.9:
                             #print("매수시작 : ", code)
                             #print("[첫시작] 매수금액 : ", current_price)
